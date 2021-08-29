b11f8ed2cf4509675dd4e927f288fc1fe210fa90
Brayan <alfabrayan12@gmail.com>
2021-08-28 19:01:18 -0600

    Division

--- reververacion.asm
+++ reververacion.asm
@@ -7,19 +7,24 @@ SECTION .text
 global  _start
  
 _start:
-
-    mov r9, 0000_0001_1000_0000b
-    mov r8, 0000_0011_0100_0000b
-    call division_float
+    mov r8, 0000_0000_0100_0111b
+    mov r9, 0000_0000_0000_0111b
+    call division
     jmp end        
 
 addition:
-    mov r15, r8
-    add r15, r9
+    mov r15d, r8d
+    add r15d, r9d
     ret 
 
 
-multiplication_float:
+substraction:
+    mov r15d, r8d
+    sub r15d, r9d
+    ret 
+
+
+multiplication:
 
     push r10
     push r11
@@ -32,87 +37,101 @@ multiplication_float:
     push rdx
 
 
-    mov r10, r8
-    mov r11, r8
-    and r10, [int_mask]
-    and r11, [float_mask]
-    shr r10, 8
+    mov r10d, r8d
+    mov r11d, r8d
+    and r10d, [int_mask]
+    and r11d, [float_mask]
+    shr r10d, 8
 
-    mov r12, r9
-    mov r13, r9
-    and r12, [int_mask]
-    and r13, [float_mask]
-    shr r12, 8
+    mov r12d, r9d
+    mov r13d, r9d
+    and r12d, [int_mask]
+    and r13d, [float_mask]
+    shr r12d, 8
     
     push r8
     push r9
 
-    mov r8, r10
-    mov r9, r12    
+    mov r8d, r10d
+    mov r9d, r12d  
 
-    call multiplication
-    mov rax, r15
+    call multiplication_aux
+    mov eax, r15d
 
-    mov r8, r10
-    mov r9, r13    
+    mov r8d, r10d
+    mov r9d, r13d    
 
-    call multiplication
-    mov rbx, r15
+    call multiplication_aux
+    mov ebx, r15d
 
-    mov r8, r11
-    mov r9, r12    
+    mov r8d, r11d
+    mov r9d, r12d    
 
-    call multiplication
-    mov rcx, r15
+    call multiplication_aux
+    mov ecx, r15d
 
-    mov r8, r11
-    mov r9, r13
+    mov r8d, r11d
+    mov r9d, r13d
 
-    call multiplication
-    mov rdx, r15
+    call multiplication_aux
+    mov edx, r15d
 
 
-    shl rax, 8
-    shr rdx, 8
+    shl eax, 8
+    shr edx, 8
 
-    add rax, rbx
-    add rax, rcx
-    add rax, rdx
+    add eax, ebx
+    add eax, ecx
+    add eax, edx
     
-    mov r15, rax
+    mov r15d, eax
 
-    pop r8
     pop r9
-    pop r10
-    pop r11
-    pop r12
-    pop r13
-    pop r14
-    pop rax
-    pop rbx
-    pop rcx
+    pop r8
     pop rdx
+    pop rcx
+    pop rbx
+    pop rax
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
 
     ret 
 
-multiplication:
+multiplication_aux:
     push rax
 
     mov eax, r8d
-    mul r9
+    mul r9d
     mov r15d, eax
-    
+
     pop rax
     ret
 
-division_float: 
-
+division: 
     push rax
-    
-    mov eax, r8d  
-    div r9      
-    mov r15, rax
-    
+    push rcx
+
+    mov eax, r8d      
+    mov r15d, 0
+div1: 
+    div r9d    
+    mov r14, 0  
+    mov r14d, eax
+    shl r14d, 8
+    add r15d, r14d
+    mov eax, edx
+div2: 
+
+    div r9d    
+    mov r14, 0  
+    mov r14d, eax
+    shr r14d, 24
+    add r15d, r14d
+
+    pop rcx
     pop rax
     
     ret
